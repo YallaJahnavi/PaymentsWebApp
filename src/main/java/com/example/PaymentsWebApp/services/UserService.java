b88129d3627e7ffbc1b27a985b450909e7ab7e96
YallaJahnavi <yallajahnavi170@gmail.com>
2025-05-13 20:02:13 +0530

    Update UserService.java

--- src/main/java/com/example/PaymentsWebApp/services/UserService.java
+++ src/main/java/com/example/PaymentsWebApp/services/UserService.java
@@ -1,35 +1,35 @@
 
-package com.example.PaymentsWebApp.services;
-
-import com.example.PaymentsWebApp.Entity.UserEntity;
-import com.example.PaymentsWebApp.dao.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public boolean registerUser(UserEntity user) {
-        if (userRepository.findByuserName(user.getUserName()) != null) {
-            return false; // User already exists
-        }
-        else {
-        userRepository.save(user);}
-        return true;
-    }
-
-    public UserEntity validateUser(String username, String password) {
-        UserEntity user = userRepository.findByuserName(username);
-        if (user != null && user.getPassword().equals(password)) {
-            return user;
-        }
-        return null;
-    }
-    public void updateUser(UserEntity user) {
-        userRepository.save(user); // will update based on user_id
-    }
-
-}
+	package com.example.PaymentsWebApp.services;
+	
+	import com.example.PaymentsWebApp.Entity.UserEntity;
+	import com.example.PaymentsWebApp.dao.UserRepository;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Service;
+	
+	@Service
+	public class UserService {
+	
+	    @Autowired
+	    private UserRepository userRepository;
+	
+	    public boolean registerUser(UserEntity user) {
+	        if (userRepository.findByuserName(user.getUserName()) != null) {
+	            return false; // User already exists
+	        }
+	        else {
+	        userRepository.save(user);}
+	        return true;
+	    }
+	
+	    public UserEntity validateUser(String username, String password) {
+	        UserEntity user = userRepository.findByuserName(username);
+	        if (user != null && user.getPassword().equals(password)) {
+	            return user;
+	        }
+	        return null;
+	    }
+	    public void updateUser(UserEntity user) {
+	        userRepository.save(user); // will update based on user_id
+	    }
+	
+	}
